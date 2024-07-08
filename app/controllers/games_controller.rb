845e2ce640a5726c2b6dabd04caae6c76dd74eb3
Jullian Calkins <jullian0426@yahoo.com>
2024-07-08 14:57:01 -0400

    Allow for the creation and deletion of games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,4 +6,30 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new
+
+    if @game.save
+      redirect_to games_path, notice: 'Game was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to games_path, notice: 'Game was successfully deleted.'
+  end
+
+  private
+
+  def game_params
+    # params.require(:game).permit()
+  end
 end
