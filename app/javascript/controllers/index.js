147f95182590130900ef8ab2cc0b88e12f370dac
Jullian Calkins <jullian0426@yahoo.com>
2024-07-18 12:00:45 -0400

    Create custom form for rank choices using a stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import RankSelectorController from "./rank_selector_controller"
+application.register("rank-selector", RankSelectorController)
+
 import RemovalsController from "./removals_controller"
 application.register("removals", RemovalsController)
 
---
+++ app/javascript/controllers/rank_selector_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="rank-selector"
+export default class extends Controller {
+  connect() {
+    this.selectedRank = null
+  }
+
+  static targets = [ "input" ]
+
+  select(event) {
+    event.preventDefault()
+    const button = event.currentTarget
+    this.selectedRank = button.dataset.rank
+
+    this.inputTarget.value = this.selectedRank
+
+    // this.buttonTargets.forEach(btn => btn.classList.remove("selected"))
+    // button.classList.add("selected")
+  }
+}
